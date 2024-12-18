e08f6eb97b7f69d21cc3bdd22a7fa28151656671
mzwoelfer <>
2024-12-18 09:08:46 +0100

    Calculate total speech time

--- WEBPAGE/src/views/HomeView.vue
+++ WEBPAGE/src/views/HomeView.vue
@@ -35,10 +35,25 @@ let searchFilter = [
   }
 ]
 
+function calculateDuration(begin: string, end: string): string {
+  const [beginHours, beginMinutes, beginSeconds] = begin.split(':').map(Number);
+  const [endHours, endMinutes, endSeconds] = end.split(':').map(Number);
+
+  const beginTimeInSeconds = beginHours * 3600 + beginMinutes * 60 + beginSeconds;
+  const endTimeInSeconds = endHours * 3600 + endMinutes * 60 + endSeconds;
+
+  const durationInSeconds = endTimeInSeconds - beginTimeInSeconds;
+  const minutes = Math.floor(durationInSeconds / 60);
+  const seconds = durationInSeconds % 60;
+
+  return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+
 onMounted(() => {
   speeches.value = speechesData.map((speech: any) => ({
     name: speech.name,
-    speak_time: `${speech.beginSpeech} - ${speech.endSpeech}`
+    speak_time: calculateDuration(speech.beginSpeech, speech.endSpeech)
   }));
 });
 </script>
